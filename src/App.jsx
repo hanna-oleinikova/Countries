9fb2c38f90593ca1e9ff8cd5dd715ddf2121e33b
Hanna Oleinikova <hanna.oleinikova@gmail.com>
2024-08-18 11:31:28 +0200

    flag fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,89 +5,151 @@ import { Input } from './components/input.component';
 function App() {
   const [data, setData] = useState([]);
   const [foundCountries, setFoundCountries] = useState([]);
+  const [countryCode, setCountryCode] = useState("");
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
     .then((response) => response.json())
     .then((data) => {
       setData(data);
-      // setFoundCountries(data);
       })
     .catch((error) => {console.log(error);})
   }, [])
-  console.log(data)
-
 
   return (
-      <>
-      <h1 className='title'>Countries</h1>
-
-      <div className='search-container'>
-      <Input data={data} setFoundCountries={setFoundCountries}/>
-
-      <div className='cont-country'>
-        <h3>You chose</h3>
-        {foundCountries.map((country) => {
-          return (
-            <h2 key={country.name.common}>{country.name.common}</h2>
-          )
-        })}
+      <div className='main-container'>
+        <h1 className='title'>Countries</h1>
+        <div className='search-container'>
+        <Input  data={data}
+                setFoundCountries={setFoundCountries}
+                setCountryCode={setCountryCode}
+        />
         </div>
-      </div>
 
-      <div className='container'>
-        <div className='cont-capital'>
-          <h3>The capital is </h3>
+
+        <div className='cont-country'>
+          <h5>Your coutry is</h5>
           {foundCountries.map((country) => {
-          return (
-            <h2 key={country.name.common}>{country.capital}</h2>
-          )
-        })}
+            return (
+              <h2 key={country.name.common}>{country.name.common}</h2>
+            )
+          })}
         </div>
-
-        <div className='cont-time'>
-          <h3>The time-zone is</h3>
-         {foundCountries.map((country) => {
-          return (
-            <h2 key={country.name.common}>{country.timezones}</h2>
-          )
-        })}
+        
+        <div className='container'>
+        
+        {/* The capital is */}
+          <div className='card'>
+          <img className='icon' src="/icons/government.png" alt="capital" />
+          <h5>The capital is </h5>
+            <div className='answer'>
+              {foundCountries.map((country) => {
+              return (
+                <h2 className='found-data' key={country.name.common}>{country.capital}</h2>
+              )
+                        })}
+            </div>
+          </div>
+        
+        {/* The time-zone is */}
+          <div className='card'>
+            <img className='icon' src="/icons/time-zone.png" alt="time-zone" />
+            <h5>The time-zone is</h5>
+              <div className='answer'>
+                {foundCountries.map((country) => {
+                return (
+                  <h3 className={country.timezones.length < 3 ? 'found-data' : 'found-data-small'} key={country.name.common}>
+                    {country.timezones.map((timezone, index) => (
+                    <span key={index}>
+                      {timezone}
+                      {index < country.timezones.length - 1 && <br />}
+                    </span>
+                    ))}
+                  </h3>
+                )
+              })}
+              </div>
+          </div>
+        
+        {/* The currency used */}
+          <div className='card'>
+            <img className='icon' src="/icons/4634986_moneys_financial_layers_money_icon.png" alt="money" />
+            <h5>The currency used</h5>
+            <div className='answer'>
+              {foundCountries.map((country) => {
+                if (country.currencies) {
+              const currencyKeys = Object.keys(country.currencies);
+              const currencyName = country.currencies[currencyKeys[0]].name;
+              const currencySymbol = country.currencies[currencyKeys[0]].symbol;
+                return (
+                  <h2 className='found-data' key={country.name.common}>{`${currencyName} (${currencySymbol})`}</h2>
+                );
+              }
+                return "Not found";
+              })}
+            </div>
+          </div>
+        
+        {/* The flag looks */}
+        <div className='card'>
+          <img className='icon' src="/icons/172466_flag_alert_icon.png" alt="flag" />
+          <h5>The flag looks</h5>
+          <div className='answer'>
+            {foundCountries.map((country) => (
+              <div key={country.name.common}>
+                {country.flags && country.flags.png ? (
+                  <img className='found-data flag-pic' src={country.flags.png} alt={`Flag of ${country.name.common}`} />
+                ) : (
+                  <p>No flag available</p>
+                )}
+              </div>
+            ))}
+          </div>
         </div>
-
-        <div className='cont-many'>
-          <h3>The currency used</h3>
-          {/* {foundCountries.map((country) => {
-          return (
-            <h2 key={country.name.common}>{Object.keys(country.currencies)[0]}</h2>
-          )
-        })} */}
+        
+        {/* They speak here */}
+          <div className='card'>
+            <img className='icon' src="/icons/speak.png" alt="speak" />
+            <h5>They speak here</h5>
+            <div className='answer'>
+              {foundCountries.map((country) => {
+                if (country.languages) {
+                  const languageCodes = Object.keys(country.languages);
+                  const languageNames = languageCodes.map((code) => country.languages[code]);
+                  return (
+                    <h2 className='found-data' key={country.name.common}>{languageNames.join(', ')}</h2>
+                  );}
+                  return "Not found";
+                })}
+            </div>
+          </div>
+        
+        {/* The country map */}
+          <div className='card'>
+          <img className='icon' src="/icons/pin_927667.png" alt="speak" />
+            <h5>The country map</h5>
+            <div className='answer'>
+              {foundCountries.map((country) => (
+                <div key={country.name.common}>
+                  {country.maps && country.maps.googleMaps ? (
+                    <div className="map-container">
+                      <a
+                        href={country.maps.googleMaps}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <img className='icon icon-map found-data' src="/icons/map.png" alt="map" />
+                      </a>
+                    </div>
+                  ) : (
+                    <div>No map available</div>
+                  )}
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-        <div className='cont-flag'>
-          <h3>The flag looks</h3>
-          {foundCountries.map((country) => {
-          return (
-            <h2 key={country.name.common}>{country.flag}</h2>
-          )
-        })}
-        </div>
-        <div className='cont-language'>
-          <h3>They speak here</h3>
-          {/* {foundCountries.map((country) => {
-          return (
-            <h2 key={country.name.common}>{Object.keys(country.languages)[0]}</h2>
-          )
-        })} */}
-        </div>
-        <div className='cont-border'>
-          <h3>The country borders</h3>
-          {foundCountries.map((country) => {
-          return (
-            <h2 key={country.name.common}>{country.borders}</h2>
-          )
-        })}
-        </div>
-      </div>
-    </>
+    </div>
   )
 }
 
--- src/components/input.component.jsx
+++ src/components/input.component.jsx
@@ -1,40 +1,57 @@
 import { useEffect, useState } from "react";
 import PropTypes from 'prop-types';
+import './input.style.scss'
 
-Input.propTypes = {
-  data: PropTypes.array.isRequired,
-  setFoundCountries: PropTypes.func.isRequired,
-};
-
-export function Input ({data, setFoundCountries}) {
+export function Input ({data, setFoundCountries, setCountryCode}) {
   const [searchString, setSearchString] = useState("");
+  const [countryList, setCountryList] = useState([]);
+
+  useEffect(() => {
+    const selectedCountry = data.find((country) => {
+      return country.name.common.toLowerCase() === searchString.toLowerCase();
+    });
+
+    if (selectedCountry) {
+      setFoundCountries([selectedCountry]);
+      setCountryCode(selectedCountry.cca2);
+    } else {
+      setFoundCountries([]);
+    }
+  }, [searchString, data]);
+
+  useEffect(() => {
+    const names = data.map((item) => item.name.common);
+    const uniqueNames = [...new Set(names)];
+    setCountryList(uniqueNames);
+  }, [data]); 
 
   function onChangeHandler(event) {
     const searchValue = event.target.value;
     setSearchString(searchValue);
   }
 
-  useEffect(() => {
-    const newArray = data.filter((i) => {
-      return i.name.common.toLowerCase().includes(searchString.toLowerCase());
-    });
-    setFoundCountries(newArray);
-
-    // if (!searchString) {
-    //   setFoundCountries(data);
-    // }
-    // setFoundCountries(newArray);
-  }, [searchString]); 
-
   return (
     <>
-    <input 
-      type="search" 
-      name="search" 
-      id="search-input"
-      placeholder='Enter the country name...' 
-      onChange={onChangeHandler} 
+      <input 
+        type="search" 
+        name="search" 
+        id="search-input"
+        placeholder='Enter the country name...' 
+        onChange={onChangeHandler}
+        list="country-list"
+        className="input-style"
       />
+      <datalist id="country-list">
+        {countryList.map((country, index) => (
+          <option key={index} value={country} />
+        ))}
+      </datalist>
     </>
   )
 }
+
+Input.propTypes = {
+  data: PropTypes.array.isRequired,
+  setFoundCountries: PropTypes.func.isRequired,
+  setCountryCode: PropTypes.func.isRequired,
+};
